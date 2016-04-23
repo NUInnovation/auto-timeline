fbf433bc3253691dac93e2db2d45c92918963580
Bomani McClendon <bomanimcclendon2017@u.northwestern.edu>
2016-04-22 23:01:09 -0500

    Added search for recent Instagram media by tag

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ var engines = require('consolidate');
 var mustache = require('mustache');
 var bodyParser = require('body-parser');
 var Twitter = require('twitter');
+var ig = require('instagram-node').instagram();
 
 var app = express();
 
@@ -24,24 +25,36 @@ var client = new Twitter({
   access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
 });
 
-function getTwitterData() {
+//Instagram API Setup
+ig.use({ access_token: process.env.INSTAGRAM_ACCESS_TOKEN });
+
+function getTwitterData(query) {
   client.get('search/tweets', {q: query}, function(error, tweets, response){
      console.log(tweets);
   });
 }
 
+function getInstagramData(query) {
+  console.log("Attempting to call instagram")
+  ig.tag_media_recent(query, function(err, medias, pagination, remaining, limit) {
+    console.log(medias)
+  });
+}
 // This will be the central function for hitting
 // each of the different platform APIs and compiling
 // the data into a coherent response to send back to
 // the front-end.
 function compileData(query) {
-  getTwitterData(query)
+  // getTwitterData(query)
+  console.log('compiling data')
+  getInstagramData(query)
 }
 
 
 app.get('/', function(req, res) {
+  console.log("Got main endpoint");
   compileData("aera16");
-	res.render('index.html')
+	res.render('home.html');
 });
 
 app.get('/create', function(req, res) {
