3dd0ce001d1e28155711006151bff87173f93e7d
Bomani McClendon <bomanimcclendon2017@u.northwestern.edu>
2016-05-08 16:07:45 -0500

    Fixed sorting function

--- filter.js
+++ filter.js
@@ -3,15 +3,10 @@ var moment = require('moment');
 function dateFilter(tweets) {
 	//Sort the tweets by created time
 	var sortedTweets = tweets.sort(function(a, b) {
-		var aTime = a.created_time;
-		var bTime = b.created_time;
-		return bTime - aTime
+		var aTime = new Date(a.created_at);
+		var bTime = new Date(b.created_at);
+		return aTime - bTime;
 	});
-
-	console.log("\n###### SHOWING SORTED TWEETS ######\n");
-	for (var i = 0; i < sortedTweets.length; i++) {
-		console.log(moment(new Date(sortedTweets[i].created_at)).calendar())
-	}
 
 }
 
