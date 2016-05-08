4dd37aff8ef908947f1c5b667e9785fde1dbcff8
Bomani McClendon <bomanimcclendon2017@u.northwestern.edu>
2016-05-08 16:31:02 -0500

    Creates a dictionary that contains counts of tweets by day

--- filter.js
+++ filter.js
@@ -8,6 +8,21 @@ function dateFilter(tweets) {
 		return aTime - bTime;
 	});
 
+	//Find counts of tweets per day
+	var daysDict = {};
+	for (var i = 0; i < sortedTweets.length; i++) {
+		var tweetTime = moment(new Date(sortedTweets[i].created_at)).format("MM/DD/YYYY");
+		if(tweetTime in daysDict) {
+			daysDict[tweetTime] += 1;
+		}
+		else {
+			daysDict[tweetTime] = 1;
+		}
+		// console.log(moment(new Date(sortedTweets[i].created_at)).calendar())
+	}
+
+	console.log(daysDict)
+
 }
 
 module.exports = {
