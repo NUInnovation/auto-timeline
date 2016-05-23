003b1d2a5ac5956016e694ec697548c87502f40e
JZhang <jzhangnu@gmail.com>
2016-05-23 17:58:43 -0500

    fixed the bug about header, still have trouble in openning in new tab

--- public/js/app.js
+++ public/js/app.js
@@ -1,8 +1,17 @@
+var url;
 function createTimeline(query) {
     $('.ex-wrapper').hide();
     $.get( "/create?query=" + query, function( res ) {
       data = res.data;
-      $('#url').html(window.location.hostname + '/timeline/' + res.id);
+      if(window.location.hostname == 'localhost'){
+        url = 'localhost:3000/timeline/' + res.id;
+        $('#url').html(url);
+      }else{
+        url = window.location.hostname + '/timeline/' + res.id;
+        $('#url').html(url);
+      }
+
+      $("#openNew").attr('href', url);
 
       var timeline = new TL.Timeline('timeline', data, {
         ga_property_id: "UA-27829802-4"
@@ -12,17 +21,13 @@ function createTimeline(query) {
       $("#timeline-header").html('');
       $('#timeline-header').show();
       $("#timeline-header").append("<div id='t-header' >Timeline For <span>#" + query + "</span></div>");
-      $('#openNew').click(function(){
-        window.open('timeline.html');
-      });
-
-      $("#timeline-header").html('');
-      $('#timeline-header').show();
 
       $("html,body").animate({ scrollTop: "700px"},1200);
       $('#urlbox').show();
     });
-
-
-
 }
+//
+// $('#openNew').click(function(){
+//   // window.open(url);
+//
+// });
